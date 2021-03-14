53d6cc5c4454211f8557f5aee1484769d7066cd5
Brandon <sugb73@gmail.com>
2021-03-13 20:08:29 -0500

    commit tree e30081cb4fe62d241ba2bc4b7986e2599e12a6e4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+  def initialize(side_1, side_2, side_3)
+      @side_1 = side_1
+      @side_2 = side_2
+      @side_3 = side_3
+  end
+
+  def kind
+    
+
+    # check for negative side lengths
+    if @side_1 < 0 || @side_2 < 0 || @side_3 < 0
+      raise TriangleError
+    end
+
+    # check 1 for tri inequality
+    if (@side_1 + @side_2) <= @side_3
+      raise TriangleError
+    end
+
+    if (@side_2 + @side_3) <= @side_1
+      raise TriangleError
+    end
+
+    if (@side_1 + @side_3) <= @side_2
+      raise TriangleError
+    end
+
+    if @side_1 == @side_2 && @side_1 == @side_3
+      if (@side_1 == 0) then raise TriangleError end
+      return :equilateral
+
+    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+      return :scalene
+    else
+      return :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Illegal Triangle"
+    end
+  end
 end
